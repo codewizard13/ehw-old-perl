8af215d247076b66c598a74c632b1cf32a6a0bf3
Eric L. Hepperle <codewizard13@gmail.com>
2022-12-28 09:13:33 -0600

    ADD 2006/ folder

---
+++ 2006/eh_fix_ceres_fields_061406_01.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+
+print "Hello, World...\n";
+
+# Program:    eh_fix_ceres_fields_061406_01.pl
+# Creator:    Eric Hepperle
+# Date:       06/14/06
+#
+# Program:    As an HP employee, I use a program called CERES.
+#             CERES is an html-based labor-reporting app that gives
+#             a list of cases for one to close.  Each case opens
+#             to its own page consisting of numerous fields.  Often,
+#             a few key fields have incorrect info.  Procedurally,
+#             to get the fields corrected requires one to contact
+#             an Onsite Coordinator (OC for short) and request the
+#             change through them.  This can be a hassle due to
+#             language barriers, incompetence, and uncertainty on
+#             their part as to whether they have permission to
+#             perform the changes.  Subsequently, my senior coworker
+#             Martin Hoel has requested that I create a program to
+#             fix those fields before they are uploaded.  That is
+#             the purpose of this program.
+#
+######################################################################
+#
+# Notes:      06/14/06, 10:32am - I will manually change the file
+#                then update to see if it works.
+#
+######################################################################
+
+# Get path or url to where the local file is located
+#   (Use list file if feasible, else process each file)
+#
+# Else use default
+#
+
+
+# Store contents of local file as a string
+#
+
+# Search the string for
